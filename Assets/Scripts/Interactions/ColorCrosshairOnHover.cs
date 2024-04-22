2bdf6636e1022730958f71d2e04d262a53f7c8a2
PCristian00 <cristianp9300@gmail.com>
2024-04-22 12:49:23 +0200

    Rinominata ColorOnHover
    Nuovo nome: ColorCrosshairOnHover

--- Assets/Scripts/Interactions/ColorCrosshairOnHover.cs
+++ Assets/Scripts/Interactions/ColorCrosshairOnHover.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ColorOnHover : MonoBehaviour, IHover
+public class ColorCrosshairOnHover : MonoBehaviour, IHover
 {
 
     public Color color;
