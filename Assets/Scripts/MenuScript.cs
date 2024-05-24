c50ce809eae2f3a30d6e28385c8d6353a4f3c289
Grabdor <gabrielebracalente@gmail.com>
2024-05-24 12:01:55 +0200

    inizio scena menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Play()
+    {
+       //incrementare la scena corrente di 1
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
