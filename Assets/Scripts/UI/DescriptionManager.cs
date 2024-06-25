b8484c0f89d0fe53aacfe6e5f7b3aadfb106aa12
PCristian00 <cristianp9300@gmail.com>
2024-06-25 18:17:18 +0200

    Rimosse stampe debug

--- Assets/Scripts/UI/DescriptionManager.cs
+++ Assets/Scripts/UI/DescriptionManager.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -23,7 +20,7 @@ public class DescriptionManager : MonoBehaviour
 
     public void ShowDescription(string title, string description)
     {
-        Debug.Log("Mostrando descrizione");
+      //  Debug.Log("Mostrando descrizione");
         gameObject.SetActive(true);
         titleText.text = title;
         descriptionText.text = description;
@@ -32,7 +29,7 @@ public class DescriptionManager : MonoBehaviour
 
     public void HideDescription()
     {
-        Debug.Log("Provando a nascondere...");
+       // Debug.Log("Provando a nascondere...");
         gameObject.SetActive(false);
         GameManager.instance.RequestInputUnblock();
     }
