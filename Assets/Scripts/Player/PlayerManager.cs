a06cb6e8cbf92fba63973b343e8235dbb978e4d8
PCristian00 <cristianp9300@gmail.com>
2024-06-26 18:38:00 +0200

    Aggiunti suoni pickup
    Aggiunti effetti sonori per il raccoglimento di Coin e Powerup

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -15,6 +15,8 @@ public class PlayerManager : MonoBehaviour
 
     public AudioClip hitSound;
     public AudioClip deathSound;
+    public AudioClip PUPickup;
+    public AudioClip CoinPickup;
     private AudioSource audioSource;
 
     public void Start()
@@ -29,35 +31,42 @@ public class PlayerManager : MonoBehaviour
         {
             EnemyBullet bullet = other.GetComponent<EnemyBullet>();
             Destroy(other.gameObject);
-            health-=bullet.damage;
+            health -= bullet.damage;
             if (health <= 0) Death();
             else audioSource.PlayOneShot(hitSound);
         }
 
-        // FORSE RIMUOVERE: I power-up non sono raccolti ma equipaggiati
-
-        if (other.CompareTag("Powerup"))
+        else
         {
-            // Attiva ogni power-up contenuto nell'oggetto in collisione
-            foreach (var powerup in other.GetComponents<IPowerup>())
+            // FORSE RIMUOVERE: I power-up non sono raccolti ma equipaggiati
+
+            if (other.CompareTag("Powerup"))
             {
-                // COOL-DOWN GENERICO: MODIFICARE IN QUALCHE MODO
-                powerup.Activate(5f);
+                // Attiva ogni power-up contenuto nell'oggetto in collisione
+                foreach (var powerup in other.GetComponents<IPowerup>())
+                {
+                    // COOL-DOWN GENERICO: MODIFICARE IN QUALCHE MODO
+                    powerup.Activate(5f);
+                }
+
+                audioSource.PlayOneShot(PUPickup);
+
+                Debug.Log("Preso power-up " + other.name);
+                Destroy(other.gameObject);
             }
 
-            Debug.Log("Preso power-up " + other.name);
-            Destroy(other.gameObject);
-        }
+            if (other.CompareTag("Coin"))
+            {
+                Coin coin = other.GetComponent<Coin>();
+                Debug.Log("Presa moneta da " + coin.value);
 
-        if (other.CompareTag("Coin"))
-        {
-            Coin coin = other.GetComponent<Coin>();
-            Debug.Log("Presa moneta da " + coin.value);
+                WalletManager.instance.wallet += coin.value;
 
-            WalletManager.instance.wallet += coin.value;
+                audioSource.PlayOneShot(CoinPickup);
+                Destroy(other.gameObject);
+            }
 
-
-            Destroy(other.gameObject);
+            
         }
     }
 
