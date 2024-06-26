34c9b293bda49f22b695e94ced16bbde3c95bf0c
PCristian00 <cristianp9300@gmail.com>
2024-06-26 16:38:15 +0200

    Modifiche a Menu
    Menu viene disattivato se attivo all'inizio.
    Utilizzato per "precaricare" le impostazioni del volume.
    Lasciare AudioMenu attivo in scena Saloon.

--- Assets/Scripts/Interactions/Menu.cs
+++ Assets/Scripts/Interactions/Menu.cs
@@ -6,17 +6,19 @@ public class Menu : MonoBehaviour
 {
     public void Start()
     {
-      //  gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     // Start is called before the first frame update
     private void OnEnable()
     {
-        GameManager.instance.RequestInputBlock();
+        if (GameManager.instance != null)
+            GameManager.instance.RequestInputBlock();
     }
 
     private void OnDisable()
     {
-        GameManager.instance.RequestInputUnblock();
+        if (GameManager.instance != null)
+            GameManager.instance.RequestInputUnblock();
     }
 }
