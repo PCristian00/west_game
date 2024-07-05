bbde4b56f93fb556c87ed28892a80a414a9e2d83
PCristian00 <cristianp9300@gmail.com>
2024-07-05 15:52:54 +0200

    Nuova interfaccia, implementazione in MainMenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,23 +4,27 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public AudioSource sparo;
-    
-    public void PlayButton(){
-        Invoke(nameof(Play), 1.5f);
+
+    public void PlayButton()
+    {
+        Invoke(nameof(Play), 0.5f);
         sparo.Play();
     }
     public void Play()
     {
         //incrementare la scena corrente di 1
-        SceneManager.LoadScene(1);
+        // SceneManager.LoadScene(1);
+        LoadingManager.instance.LoadSceneFromIndex(1);
     }
 
-    public void ExitButton(){
+    public void ExitButton()
+    {
         Invoke(nameof(Exit), 1.5f);
         sparo.Play();
     }
 
-    public void Exit(){
+    public void Exit()
+    {
         Debug.Log("Quitting game");
         Application.Quit();
     }
