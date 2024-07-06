4ad1b52e1c08b112903b08a0672e988ec692969c
PCristian00 <cristianp9300@gmail.com>
2024-07-06 19:39:09 +0200

    Fix gerarchia Canvas
    Il caricamento è tornato in primo piano.
    Le skill sono di nuovo visibili.

--- Assets/Scripts/UI/SkillUI.cs
+++ Assets/Scripts/UI/SkillUI.cs
@@ -4,10 +4,16 @@ public class SkillUI : MonoBehaviour
 {
     void Start()
     {
+        
+    }
+
+    private void Update()
+    {
         if (!SkillManager.instance)
         {
             Debug.Log("NO SKILLLL");
             gameObject.SetActive(false);
         }
+
     }
 }
